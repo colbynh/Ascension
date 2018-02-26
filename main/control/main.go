f5bd5d14b7ee7802f8e0f9feed97c2a18a73a5a5
colbynh <guitaristcolby@gmail.com>
2018-02-26 13:21:12 -0400

    Add better tests to main

--- main/control/main.go
+++ main/control/main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"strconv"
 	"SDK/philips"
 	"fmt"
 	"time"
+	"math/rand"
 )
 
 func main() {
-	// Initial integration test
+	// Initial integration tests
 	allLights, err := lights.GetAll()
 	if err != nil {
 		fmt.Println("Getall error: "+err.Error())
 	}
-	time.Sleep(time.Second *2)
-	for _, l := range allLights {
-		time.Sleep(time.Second *2)
-		l.State.On = false
-		err = lights.SetState(l)
-		fmt.Println(err)
+
+	RenameAllLights(allLights)
+	RandColors(allLights)
+}
+
+func RenameAllLights(lts []lights.ColorHue) {
+	for i, l := range lts {
+		l.Name = "Colby"+strconv.Itoa(i)
+		err := lights.Rename(l)
+		if err != nil {
+			fmt.Println(err)
+		}
+		
+	}
+	newLts, err := lights.GetAll()
+	if err != nil {
+		fmt.Println("Getall error: "+err.Error())
+	}
+	
+	for _, l := range newLts {
+		fmt.Println("New Light: ", l.Name)
 	}
 }
+
+func RandColors(lts []lights.ColorHue) {
+	for i := 0; i < 2; i++ {
+		for _, l := range lts {
+			l.State.Hue = rand.Intn(100000)
+			err := lights.SetState(l)
+			if err != nil {
+				fmt.Println(err)
+			}
+			time.Sleep(time.Second *1)
+		}
+	}
+}
+
